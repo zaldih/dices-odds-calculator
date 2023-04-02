fec25baa665aa962c05d1502cb2ff74c56f38dad
zaldih <imzaldih@gmail.com>
2023-04-02 13:07:48 +0200

    style: add footer, improve navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './features/navbar/navbar.component';
 import { SharedModule } from './shared/shared.module';
+import { FooterComponent } from './features/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [AppComponent, NavbarComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
